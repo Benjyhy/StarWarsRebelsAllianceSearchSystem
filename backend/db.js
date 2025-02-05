8965ea70e849e036078dc9174e2947725ba734c0
Benjamin Ramet <ramet.benj@gmail.com>
2025-02-05 17:05:18 +0100

    Add authentication to backend

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const users = {
+    luke: {
+        username: 'luke',
+        password: '$2y$10$BrxbvcwYiX0u4w0gg7PHiOFJgQvCmpO5nyko6DNq8lT6rIuHTsdFC',
+        //DadSucks
+    }
+}
+
+module.exports = {users};
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,21 @@
 const Hapi = require('@hapi/hapi');
 const {SwapiRoutes, SwapiBaseUrl} = require('./constants');
 const {getIdFromUrl, getTypeFromUrl} = require('./helper');
+const Bcrypt = require('bcrypt');
+const {users} = require('./db');
+
+const validate = async (request, username, password) => {
+
+    const user = users[username];
+    if (!user) {
+        return { credentials: null, isValid: false };
+    }
+
+    const isValid = await Bcrypt.compare(password, user.password);
+    const credentials = { id: user.id, name: user.name };
+
+    return { isValid, credentials };
+};
 
 const init = async () => {
 
@@ -10,6 +25,9 @@ const init = async () => {
         port: 3001,
         host: 'localhost'
     });
+
+    await server.register(require('@hapi/basic'));
+    server.auth.strategy('simple', 'basic', { validate });
 
     server.route({
         method: 'GET',
@@ -23,6 +41,9 @@ const init = async () => {
     server.route({
         method: 'GET',
         path: '/search',
+        options: {
+            auth: 'simple',
+        },
         handler: async (req, h) => {
             try {
                 const filters = req.query.types.split(',');
